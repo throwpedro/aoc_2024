e4fee19c03533e96b74553746099b3a0c7e2537c
Thor <thordoor89@hotmail.com>
2024-12-03 20:24:53 +0100

    feat: day_03 part 2

--- day_03/main.js
+++ day_03/main.js
@@ -1,27 +1,47 @@
 const main = async () => {
-    const res = await solve();
-    // console.log(res);
+  const res = await solve();
+  console.log(res);
 }
 
 const solve = async () => {
-    const file = Bun.file("input.txt")
-    const inputStr = await file.text();
-    const reg = /mul\(\d{1,3},\d{1,3}\)/g;
-    const regdont = /don\'t/g;
-    const regdo = /do/;
-    // first pass find chunks of dos and don'ts
-    // const arr = inputStr.split(/[dont't, do]+/)
-    const matches = inputStr.match(reg);
-    let sum = 0;
-    matches.forEach((entry) => {
-        const comma = entry.indexOf(',');
-        const start = entry.indexOf('(') + 1;
-        const end = entry.indexOf(')');
-        const firstNum = Number(entry.slice(start, comma));
-        const lastNum = Number(entry.slice(comma + 1, end));
-        sum += firstNum * lastNum;
-    })
-    return sum;
+  const file = Bun.file("input.txt")
+  const inputStr = await file.text();
+  const reg = /mul\((\d+),(\d+)\)/g;
+
+  // part 2
+  let active = true;
+  let sections = inputStr.split(/(do\(\)|don't\(\))/);
+  let sum2 = 0;
+
+  sections.forEach((section) => {
+    if (section === "do()") {
+      active = true;
+    } else if (section === "don't()") {
+      active = false;
+    } else if (active) {
+      let match;
+      while ((match = reg.exec(section)) !== null) {
+        console.log(match[1], match[2]);
+        const x = parseInt(match[1], 10);
+        const y = parseInt(match[2], 10);
+        sum2 += x * y;
+      }
+    }
+  });
+  return sum2;
+
+  // part 1
+  const matches = inputStr.match(reg);
+  let sum = 0;
+  matches.forEach((entry) => {
+    const comma = entry.indexOf(',');
+    const start = entry.indexOf('(') + 1;
+    const end = entry.indexOf(')');
+    const firstNum = Number(entry.slice(start, comma));
+    const lastNum = Number(entry.slice(comma + 1, end));
+    sum += firstNum * lastNum;
+  })
+  return sum;
 }
 
 main();
